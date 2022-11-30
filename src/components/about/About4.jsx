39590f0c83940c8c8698806eb653dd6f3f521a19
Camilo Gálvez Vidal <cgalvez@tactech.cl>
2022-11-29 23:22:39 -0300

    Update about component with new image and information

--- src/components/about/About4.jsx
+++ src/components/about/About4.jsx
@@ -9,41 +9,33 @@ function About4() {
     <div className="row gy-5">
       <div className="col-xl-6 text-lg-start d-sm-flex d-none justify-content-center wow fadeInDown" data-wow-duration="1.5s" data-wow-delay="200ms">
         <div className="about3-image-area">
-          <img src="assets/images/bg/about3-bg.png" className="about3-image" alt="image" />
-          <img src="assets/images/bg/abt-img-text1.svg" className="abt-img-text1" alt="image" />
-          <img src="assets/images/bg/abt-img-text2.svg" className="abt-img-text2" alt="image" />
-          <span className="years">05</span>
+          <img src="assets/images/bg/about-fabia.png" className="about3-image" alt="image" />
         </div>
       </div>
       <div className="col-xl-6 d-flex justify-content-xl-end flex-column justify-content-center text-xl-start text-center">
         <div className="section-title-area sibling4">
           <div className="marquee">
             <div>
-              <span>About Corelaw</span>
-              <span>About Corelaw</span>
+              <span>¿Quiénes Somos?</span>
+              <span>¿Quiénes Somos?</span>
             </div>
           </div>
           <div className="section-title text-xl-start text-center">
-            <span>GET TO KNOW US</span>
-            <h2>Let you know who we are  learn about our firm.</h2>
+            <span>¿QUIENES SOMOS?</span>
+            <h2>Nuestra misión y compromiso con la gente</h2>
           </div>
         </div>
         <div className="about3-content sibling2">
-          <p className="para">In consequat tincidunt turpis sit amet imperdiet. Praesent Class officelan nonatoureanor mauris laoreet, iaculis libero quis.Curabitur et tempus eri 
-            consequat tincidunt turpis sit amet imperdiet. Praesent nonatourean olei
-            aptent taciti sociosqu ad litora torquent per.</p>
+          <p className="para">Somos un grupo de profesionales liderados por la Abogada Fabia Macarena Rojas Herrera y
+            nos orientamos en asesorar a nuestros clientes con un gran compromiso humano y profesional, de manera
+            responsable con el entorno y con los problemas que cada uno de ellos nos confía. Nuestro compromiso es:
+          </p>
           <ul className="about3-list sibling2">
-            <li>Aenean vitae pharetra leo. Aliquam poriana told gotten guning.</li>
-            <li>In aliquet, quam vitae blandit temporg all of person are thisis.</li>
-            <li>Aenean volutpat enim vitae tincidunta we are got this timeng.</li>
-            <li>Felis at venenatis porttitor, nunc arcua law is best of this law.</li>
+            <li>La búsqueda activa de soluciones jurídicas acordes a las problemáticas actuales.</li>
+            <li>Generar soluciones de calidad y en concordancia con la normativa legal vigente.</li>
+            <li>Capacitación constante en diveras áreas del derecho, en pro de entregar un servicio más especializado.</li>
+            <li>Entregar un asesoramiento jurídico altamente calificado, pero con la intención de satisfacer la problemática del cliente.</li>
           </ul>
-          <div className="d-flex justify-content-xl-start justify-content-center align-items-center flex-wrap gap-4">
-            <div className="about3-author">
-              <img src="assets/images/bg/about3-author.png" className="author-image" alt="image" />
-            </div>
-            <img src="assets/images/icons/signature3.svg" alt="image" />
-          </div>
         </div>
       </div>
     </div>
